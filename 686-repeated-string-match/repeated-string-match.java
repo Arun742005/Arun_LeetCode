47ef8087f69c556ac4c362c2087be9867eebeeb1
Arunkumar S <arunkumarpp51@gmail.com>
2025-07-22 21:15:25 +0530

    Time: 1040 ms (5.12%) | Memory: 42.2 MB (78.76%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int count=0;
+        while(sb.length() < b.length()){
+            sb.append(a);
+            count++;
+        }
+        if(isSubstring(sb.toString(),b)){
+         return count;
+        }
+
+        sb.append(a);
+        count++;
+        if(isSubstring(sb.toString(),b)){
+            return count;
+        }
+        return -1;
+    }
+    public boolean isSubstring(String str,String sub){
+        for(int i=0;i<=str.length()-sub.length(); i++){
+
+            int j=0;
+            while(j<sub.length() && str.charAt(i+j) == sub.charAt(j)){
+                j++;
+            }
+
+            if(j==sub.length()){
+                return true;
+            }
+        }
+        return false;
+    }
+}
