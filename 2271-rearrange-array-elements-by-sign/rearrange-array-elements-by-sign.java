2a25f1b688f4d9c3e87c28c53a7d14755b2fee5f
Arunkumar S <arunkumarpp51@gmail.com>
2025-09-23 16:02:50 +0530

    Time: 3 ms (100.00%) | Memory: 85.6 MB (8.31%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,22 +1,37 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        List<Integer>pos = new ArrayList<>();
-        List<Integer>neg = new ArrayList<>();
+
+        int n = nums.length;
+        int[] ans = new int[n];
+        int posindex =0 , negindex =1;
 
         for(int num : nums){
-            if(num > 0 )
-                pos.add(num);
-            else
-                neg.add(num); 
+            if(num >0){
+                ans[posindex] = num;
+                posindex +=  2;
+            }else{
+                ans[negindex] = num;
+                negindex += 2;
+            }
         }
-        int[] result = new int[nums.length];
-        int i = 0, p = 0, n = 0;
+        return ans;
+        // List<Integer>pos = new ArrayList<>();
+        // List<Integer>neg = new ArrayList<>();
 
-        while(p < pos.size() && n < neg.size()){
-            result[i++] = pos.get(p++);
-            result[i++] = neg.get(n++);
+        // for(int num : nums){
+        //     if(num > 0 )
+        //         pos.add(num);
+        //     else
+        //         neg.add(num); 
+        // }
+        // int[] result = new int[nums.length];
+        // int i = 0, p = 0, n = 0;
 
-        }
-        return result;
+        // while(p < pos.size() && n < neg.size()){
+        //     result[i++] = pos.get(p++);
+        //     result[i++] = neg.get(n++);
+
+        // }
+        // return result;
     }
 }
