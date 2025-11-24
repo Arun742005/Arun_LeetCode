945b80b2771db363b0b9b80fce42307e371b5858
Arunkumar S <arunkumarpp51@gmail.com>
2025-11-24 16:05:35 +0530

    Time: 28 ms (89.50%) | Memory: 47.8 MB (34.06%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -1,27 +1,27 @@
 class Solution {
     public String removeStars(String s) {
-        Stack<Character> st = new Stack<>();
+    //     Stack<Character> st = new Stack<>();
         
-        for(char c : s.toCharArray()){
+    //     for(char c : s.toCharArray()){
+    //         if(c == '*'){
+    //             st.pop();
+    //         }else{
+    //             st.push(c);
+    //         }
+    //     }
+    //     StringBuilder sb = new StringBuilder();
+    //     for(char c: st) sb.append(c);
+
+    //     return sb.toString();
+
+        StringBuilder sb = new StringBuilder();
+        for(char c: s.toCharArray() ){
             if(c == '*'){
-                st.pop();
+                sb.deleteCharAt(sb.length() -1);
             }else{
-                st.push(c);
+                sb.append(c);
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for(char c: st) sb.append(c);
-
         return sb.toString();
-
-        // StringBuilder sb = new StringBuilder();
-        // for(char c: s.toCharArray() ){
-        //     if(c == '*'){
-        //         sb.deleteCharAt(sb.length() -1);
-        //     }else{
-        //         sb.append(c);
-        //     }
-        // }
-        // return sb.toString();
     }
 }
